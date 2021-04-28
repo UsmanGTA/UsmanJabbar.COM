313f98a6786e0386b970d79b7aa9d16eefe08848
UsmanGTA <hi@usmanjabbar.com>
2021-04-28 08:26:00 +0500

    Added working menu

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,39 @@
-window.onscroll = function(e) {
-	let navbar = document.getElementById('navbar');
+// Menu Animations
+let menuBtn = document.getElementById('menu-btn-check');
+let menuCon = document.getElementById('menu-container');
+let naviBar = document.getElementById('navbar');
+let menuBar = document.getElementsByClassName('line');
 
-	if (this.oldScroll > this.scrollY) {
-		navbar.style.top = "0";
+menuBtn.onclick = () => {
+	if (menuBtn.checked) {
+		menuCon.style.height = "100vh"
+		naviBar.style.backgroundColor = "black"
+		naviBar.style.color = "white"
+		for (const bar of menuBar) {
+			bar.style.backgroundColor = "white"
+		}
 	} else {
-		navbar.style.top = "-4.5rem";
+		menuCon.style.height = "4.5rem"
+		naviBar.style.backgroundColor = "white"
+		naviBar.style.color = "black"
+		for (const bar of menuBar) {
+			bar.style.backgroundColor = "black"
+		}
 	}
+}
 
-	this.oldScroll = this.scrollY;
+window.onscroll = function() {
+	if (!menuBtn.checked) {
+		if (this.oldScroll > this.scrollY) { naviBar.style.top = "0"; }
+		else { naviBar.style.top = "-4.5rem"; }
+		this.oldScroll = this.scrollY;
+	}
+}
+
+let menuItems = document.querySelectorAll('#navbar a')
+for (const menuItem of menuItems) {
+	menuItem.addEventListener('click', () => {
+		menuBtn.checked = false;
+		menuBtn.onclick()
+	})
 }
